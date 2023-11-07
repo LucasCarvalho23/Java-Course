6b8005740de6dd37d13327fa0fa11f73dc68d276
Lucas Carvalho <95986277+LucasCarvalho23@users.noreply.github.com>
2023-11-06 23:33:11 -0300

    Double variable
    Double variable

---
+++ 001. Padawan/001. Hello World (Double variable)/src/Main.java
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		double piNum = 3.14159265359;
+		
+		System.out.println (piNum);
+		System.out.print("O valor de PI é: ");
+		System.out.printf("%.2f\n", piNum);
+		
+
+	}
+
+}
